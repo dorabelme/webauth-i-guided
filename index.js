7d7649145e688a37ef65568e2f1afa0cdce3cde1
Dora Belme <contact.dorabelme@gmail.com>
2019-09-16 12:46:10 -0400

    added bcrypt

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const bcrypt = require('bcryptjs');
+
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
 
@@ -51,6 +53,16 @@ server.get('/api/users', (req, res) => {
     })
     .catch(err => res.send(err));
 });
+
+server.get('/hash', (req, res) => {
+  const name = req.query.name;
+
+  // hash the name
+  const hash = bcrypt.hashSync(name, 14);
+  req.query.name = hash; // use bcrypt to hash the name
+  
+  res.send(`the hash for ${name} is ${hash}`);
+})
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
