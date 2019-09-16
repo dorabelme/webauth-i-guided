e67df071b5a5b3f5a6ef2ef8f2e831c7fd1839b8
Dora Belme <contact.dorabelme@gmail.com>
2019-09-16 13:26:54 -0400

    checking password at login

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/register', (req, res) => {
-  let user = req.body;
+  let {username, password} = req.body;
 
-  Users.add(user)
+  const hash = bcrypt.hashSync(password, 8)
+
+  Users.add({username, password: hash})
     .then(saved => {
       res.status(201).json(saved);
     })
@@ -33,12 +35,12 @@ server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
-    .first()
+    .first() 
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: 'You cannot pass' });
       }
     })
     .catch(error => {
